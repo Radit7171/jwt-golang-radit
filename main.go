eeabb40137bd3e563ed5320d6acfea324310c8a4
Radit7171 <raditjal717@email.com>
2025-08-21 15:14:41 +0700

    fix heroku run go

--- main.go
+++ main.go
@@ -1,34 +1,29 @@
 package main
 
 import (
-	"booking-bengkel-smkn1depok/database"
-	"booking-bengkel-smkn1depok/models"
-	"booking-bengkel-smkn1depok/routes"
-	"fmt"
-	"os"
-
-	"github.com/gofiber/fiber/v2"
+    "booking-bengkel-smkn1depok/database"
+    "booking-bengkel-smkn1depok/models"
+    "booking-bengkel-smkn1depok/routes"
+    "github.com/gofiber/fiber/v2"
+    "os"
 )
 
 func main() {
-	// koneksi database
-	database.ConnectDB()
+    app := fiber.New()
 
-	// migrasi tabel
-	database.DB.AutoMigrate(&models.Booking{}, &models.Servis{}, &models.Teknisi{}, &models.User{})
+    // koneksi database
+    database.ConnectDB()
 
-	// init fiber
-	app := fiber.New()
+    // migrasi tabel
+    database.DB.AutoMigrate(&models.Booking{}, &models.Servis{}, &models.Teknisi{}, &models.User{})
 
-	// daftar routes
-	routes.SetupRoutes(app)
+    // daftar routes
+    routes.SetupRoutes(app)
 
-	// ambil PORT dari Render / default 3000
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-	}
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "3000"
+    }
 
-	fmt.Printf("Server running on port %s 🚀\n", port)
-	app.Listen(":" + port)
+    app.Listen(":" + port)
 }
